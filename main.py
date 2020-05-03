0cc68b09bdc8f31f694011b807a6c29fb1ec3316
Mjkli <Mjkli@users.noreply.github.com>
2020-05-02 23:36:11 -0700

    updating

--- main.py
+++ main.py
@@ -1,19 +1,35 @@
-
-
 class Location:
+    name = None
+
     def __init__(self):
         self.name = None
         self.hotel = None
         self.hotelcost = 0
         self.totalcost = self.hotelcost
+        self.nextLocation = None
+
+    def Location(self):
+        pass
 
 
-numlocations = int(raw_input("How many locations do you want to travel to?\n"))
+    def nlocation(self, Location):
+        self.nextLocation = Location
+
+
+numlocations = int(input("How many locations do you want to travel to?\n"))
+
 
 locations = [Location() for i in range(numlocations)]
 
-locations[0].name = raw_input("What is the first location you want to travel?\n")
 
-locations[0].hotelcost = 300
-print(locations[0].hotelcost)
+for i in range(numlocations):
+    if i == 0:
+        print("What is your starting location?")
+    else:
+        print("What is the " + str(i) + " place?")
 
+    locations[i].name = input()
+
+
+for i in range(numlocations):
+    print(Location.name + "\n" + Location.nextLocation + "\n")
