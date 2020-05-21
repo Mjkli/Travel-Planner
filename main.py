1a8ea226805f8d3192f84fa973e33df09fd573a0
Mjkli <Mjkli@users.noreply.github.com>
2020-05-21 11:38:48 -0700

    Testing

--- main.py
+++ main.py
@@ -2,23 +2,12 @@ class Location:
 
     def __init__(self):
         self.name = None
-        self.hotel = None
-        self.hotelcost = 0
-        self.totalcost = self.hotelcost
-        self.nextLocation = None
-
-    def Location(self):
-        pass
-
-
-    def nlocation(self, Location):
-        self.nextLocation = Location
-
-    def setname(self, name):
+    def setname(self,name):
         self.name = name
 
 
-numlocations = int(input("How many locations do you want to travel to?\n")) + 1
+
+numlocations = int(input("How many locations do you want to travel to?(city,country)\n")) + 1
 
 
 locations = [Location() for i in range(numlocations)]
@@ -31,5 +20,13 @@ for i in range(numlocations):
         print("What is the " + str(i) + " place?")
 
     locations[i].setname(input())
+
+for i in range(numlocations):
+    print(locations[i].name)
+
+
+    #test
+
+
 
 
